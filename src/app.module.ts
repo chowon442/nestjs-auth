6f92ee8ceab488dcf820d134730f7ac8e12e87b4
chowon442 <chowon442@gmail.com>
2025-02-06 07:50:40 +0900

    feat: auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,10 @@ import { AuthorModule } from './author/author.module';
 import { Author } from './author/entity/author.entity';
 import { GenreModule } from './genre/genre.module';
 import { Genre } from './genre/entity/genre.entity';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { User } from './user/entity/user.entity';
+import { envVariableKeys } from './common/const/env.const';
 
 @Module({
     imports: [
@@ -22,17 +26,20 @@ import { Genre } from './genre/entity/genre.entity';
                 DB_USERNAME: Joi.string().required(),
                 DB_PASSWORD: Joi.string().required(),
                 DB_DATABASE: Joi.string().required(),
+                HASH_ROUNDS: Joi.number().required(),
+                ACCESS_TOKEN_SECRET: Joi.string().required(),
+                REFRESH_TOKEN_SECRET: Joi.string().required(),
             }),
         }),
         TypeOrmModule.forRootAsync({
             useFactory: (configService: ConfigService) => ({
-                type: configService.get<string>('DB_TYPE') as 'postgres',
-                host: configService.get<string>('DB_HOST'),
-                port: configService.get<number>('DB_PORT'),
-                username: configService.get<string>('DB_USERNAME'),
-                password: configService.get<string>('DB_PASSWORD'),
-                database: configService.get<string>('DB_DATABASE'),
-                entities: [Book, BookDetail, Author, Genre],
+                type: configService.get<string>(envVariableKeys.dbType) as 'postgres',
+                host: configService.get<string>(envVariableKeys.dbHost),
+                port: configService.get<number>(envVariableKeys.dbPort),
+                username: configService.get<string>(envVariableKeys.dbUsername),
+                password: configService.get<string>(envVariableKeys.dbPassword),
+                database: configService.get<string>(envVariableKeys.dbDatabase),
+                entities: [Book, BookDetail, Author, Genre, User],
                 synchronize: true,
             }),
             inject: [ConfigService]
@@ -40,6 +47,8 @@ import { Genre } from './genre/entity/genre.entity';
         BookModule,
         AuthorModule,
         GenreModule,
+        UserModule,
+        AuthModule,
     ],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Headers, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './strategy/local.strategy';
+import { JwtAuthGuard } from './strategy/jwt.strategy';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('register')
+    // authorization: Basic $token
+    registerUser(@Headers('authorization') token: string) {
+        return this.authService.register(token);
+    }
+
+    @Post('login')
+    loginUser(@Headers('authorization') token: string) {
+        return this.authService.login(token);
+    }
+
+    @Post('token/access')
+    async rotateAccessToken(@Headers('authorization') token: string) {
+        const payload = await this.authService.parseBearerToken(token, true);
+
+        return {
+            accessToken: await this.authService.issueToken(payload, false),
+        }
+    }
+}
